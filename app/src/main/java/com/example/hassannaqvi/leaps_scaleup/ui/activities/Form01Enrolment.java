3a40c336eb94651b9cc0a08eaa34eee68a72fc10
ramsha.ahmed <ramshasaeed2016@gmail.com>
2018-09-19 12:10:33 +0500

    Added Form01 Activity

---
+++ app/src/main/java/com/example/hassannaqvi/leaps_scaleup/ui/activities/Form01Enrolment.java
@@ -0,0 +1,13 @@
+package com.example.hassannaqvi.leaps_scaleup.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Form01Enrolment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form01_enrolment);
+    }
+}
