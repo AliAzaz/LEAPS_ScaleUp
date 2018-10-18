34fe35e08efc7110445d04559c19be1af205ad09
ali.azaz <ali.azaz.alam@hotmail.com>
2018-10-18 11:48:39 +0500

    work on info activity

---
+++ app/src/main/java/com/example/hassannaqvi/leaps_scaleup/ui/activities/InfoActivity.java
@@ -0,0 +1,18 @@
+package com.example.hassannaqvi.leaps_scaleup.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.hassannaqvi.leaps_scaleup.R;
+import com.example.hassannaqvi.leaps_scaleup.databinding.ActivityInfoBinding;
+
+public class InfoActivity extends AppCompatActivity {
+
+    ActivityInfoBinding bi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
