c728481b48b66b43424c9e0b6b00ac25ef45cd5c
ali.azaz <ali.azaz.alam@hotmail.com>
2018-10-24 10:43:19 +0500

    commit

---
+++ app/src/main/java/com/example/hassannaqvi/leaps_scaleup/ui/activities/Form05IdBBActivity.java
@@ -0,0 +1,43 @@
+package com.example.hassannaqvi.leaps_scaleup.ui.activities;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.hassannaqvi.leaps_scaleup.R;
+import com.example.hassannaqvi.leaps_scaleup.databinding.ActivityForm05IdBBBinding;
+
+public class Form05IdBBActivity extends AppCompatActivity {
+
+    ActivityForm05IdBBBinding bi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_form05_id_b_b);
+        bi.setCallback(this);
+
+    }
+
+    public void BtnContinue() {
+        startActivity(new Intent(this, Form05IdBCActivity.class));
+        if (formValidation()) {
+            try {
+                SaveDraft();
+                if (UpdateDB()) {
+                    startActivity(new Intent(getApplicationContext(), EndingActivity.class).putExtra("complete", true));
+                } else {
+                    Toast.makeText(this, "Error in updating db!!", Toast.LENGTH_SHORT).show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void BtnEnd() {
+        startActivity(new Intent(this, EndingActivity.class).putExtra("complete", false));
+    }
+}
