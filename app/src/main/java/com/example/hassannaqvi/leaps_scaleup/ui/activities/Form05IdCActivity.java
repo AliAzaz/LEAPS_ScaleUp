af2482cfb6663ef61adc3c6bcef144b0713404de
ali.azaz <ali.azaz.alam@hotmail.com>
2018-10-25 17:09:16 +0500

    FORM VALIDATION on Form 5 DONE.

--- app/src/main/java/com/example/hassannaqvi/leaps_scaleup/ui/activities/Form05IdCActivity.java
+++ app/src/main/java/com/example/hassannaqvi/leaps_scaleup/ui/activities/Form05IdCActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.hassannaqvi.leaps_scaleup.R;
 import com.example.hassannaqvi.leaps_scaleup.databinding.ActivityForm05IdCBinding;
+import com.example.hassannaqvi.leaps_scaleup.validation.validatorClass;
 
 public class Form05IdCActivity extends AppCompatActivity {
 
@@ -21,20 +23,31 @@ public class Form05IdCActivity extends AppCompatActivity {
 
     }
 
+
     public void BtnContinue() {
-        startActivity(new Intent(this, Form02HHPart_2_HI_SE.class));
-      /*  if (formValidation()) {
-        try {
+
+        if (formValidation()) {
             SaveDraft();
-                if (UpdateDB()) {
-            startActivity(new Intent(getApplicationContext(), EndingActivity.class).putExtra("complete", true));
-                } else {
-            Toast.makeText(this, "Error in updating db!!", Toast.LENGTH_SHORT).show();
-                }
-        } catch (JSONException e) {
-            e.printStackTrace();
+            if (UpdateDB()) {
+                startActivity(new Intent(getApplicationContext(), EndingActivity.class));
+            } else {
+                Toast.makeText(this, "Error in updating db!!", Toast.LENGTH_SHORT).show();
+            }
         }
-        }*/
+
+    }
+
+    private boolean UpdateDB() {
+        return true;
+    }
+
+    private void SaveDraft() {
+
+    }
+
+    private boolean formValidation() {
+
+        return validatorClass.EmptyCheckingContainer(this, bi.flgGrpf05C01);
     }
 
     public void BtnEnd() {
