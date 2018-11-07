5ecc74d2bad996347adc00f8b66f8fbe7ecb814e
ramsha.ahmed <ramshasaeed2016@gmail.com>
2018-11-07 15:34:37 +0500

    Added Form 04 EF section e Skip Pattern

--- app/src/main/java/com/example/hassannaqvi/leaps_scaleup/ui/activities/Form04_EF_E.java
+++ app/src/main/java/com/example/hassannaqvi/leaps_scaleup/ui/activities/Form04_EF_E.java
@@ -7,6 +7,9 @@ import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.example.hassannaqvi.leaps_scaleup.JSON.GeneratorClass;
@@ -15,25 +18,41 @@ import com.example.hassannaqvi.leaps_scaleup.RMOperations.crudOperations;
 import com.example.hassannaqvi.leaps_scaleup.data.DAO.FormsDAO;
 import com.example.hassannaqvi.leaps_scaleup.databinding.ActivityForm04EfABinding;
 import com.example.hassannaqvi.leaps_scaleup.databinding.ActivityForm04EfEBinding;
+import com.example.hassannaqvi.leaps_scaleup.validation.ClearClass;
 import com.example.hassannaqvi.leaps_scaleup.validation.validatorClass;
 
 import org.json.JSONObject;
 
+import java.lang.reflect.Field;
 import java.util.concurrent.ExecutionException;
 
 import static com.example.hassannaqvi.leaps_scaleup.ui.activities.LoginActivity.db;
 
-public class Form04_EF_E extends AppCompatActivity {
+public class Form04_EF_E extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener{
 ActivityForm04EfEBinding bi;
+    int ls04e1c01, ls04e1c02, ls04e1c03, ls04e1c04, ls04e1c05, ls04e1c06, ls04e1c07, ls04e1c08, ls04e1c09, ls04e1c10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         bi = DataBindingUtil.setContentView(this,R.layout.activity_form04_ef_e);
         bi.setCallback(this);
         validatorClass.setScrollViewFocus(bi.efEScrollview);
+        attachingListners();
     }
     public void BtnEnd() {
         startActivity(new Intent(this, EndingActivity.class).putExtra("complete", false));
+    }
+    public void attachingListners(){
+        bi.ls04e1c01.setOnCheckedChangeListener(this);
+        bi.ls04e1c02.setOnCheckedChangeListener(this);
+        bi.ls04e1c03.setOnCheckedChangeListener(this);
+        bi.ls04e1c04.setOnCheckedChangeListener(this);
+        bi.ls04e1c05.setOnCheckedChangeListener(this);
+        bi.ls04e1c06.setOnCheckedChangeListener(this);
+        bi.ls04e1c07.setOnCheckedChangeListener(this);
+        bi.ls04e1c08.setOnCheckedChangeListener(this);
+        bi.ls04e1c09.setOnCheckedChangeListener(this);
+        bi.ls04e1c10.setOnCheckedChangeListener(this);
     }
 
     public void BtnContinue() {
@@ -85,4 +104,56 @@ ActivityForm04EfEBinding bi;
 
         Toast.makeText(this, "You can't go back", Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        RadioButton radioButton = findViewById(group.getCheckedRadioButtonId());
+        String radiogrpID = getResources().getResourceEntryName(group.getId());
+        if(radioButton.getText().toString().equals(getResources().getIdentifier(radiogrpID + "pattern", "id", getPackageName()))){
+            try {
+                setVariable(radiogrpID,2);
+                showHideLevel2();
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+
+        }else if(radioButton.getText().toString().equals(getString(R.string.selfcorrect))){
+            try {
+                setVariable(radiogrpID,1);
+                showHideLevel2();
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }else {
+            try {
+                setVariable(radiogrpID,0);
+                showHideLevel2();
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void setVariable(String variableName, int value)
+            throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+        Field field = this.getClass().getDeclaredField(variableName);
+        field.setAccessible(true);
+        field.set(this, value);
+    }
+    public void showHideLevel2(){
+        int total = ls04e1c01+ ls04e1c02+ ls04e1c03+ ls04e1c04+ ls04e1c05+ ls04e1c06+ ls04e1c07+ ls04e1c08+ ls04e1c09+ ls04e1c10;
+        if(total >= 5){
+            bi.level2.setVisibility(View.VISIBLE);
+        }else {
+            bi.level2.setVisibility(View.GONE);
+            ClearClass.ClearAllFields(bi.level2,false);
+        }
+    }
+
 }
