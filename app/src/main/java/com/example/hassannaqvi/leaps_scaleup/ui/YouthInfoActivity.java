a83b250e190d90fd1bb10f28e5602dbb1d220c0c
ali.azaz <ali.azaz.alam@hotmail.com>
2020-04-24 12:56:44 +0500

    commit

--- app/src/main/java/com/example/hassannaqvi/leaps_scaleup/ui/YouthInfoActivity.java
+++ app/src/main/java/com/example/hassannaqvi/leaps_scaleup/ui/YouthInfoActivity.java
@@ -168,9 +168,7 @@ public class YouthInfoActivity extends AppCompatActivity {
                 Toast.makeText(this, "Youth ID not found!!", Toast.LENGTH_SHORT).show();
             }
 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
+        } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
 
